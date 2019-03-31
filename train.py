c831a50b73d5c4df8ba0d25fcc4d15912bc871ad
orris27 <gsorris27@gmail.com>
2019-04-01 00:16:38 +0800

    ok

--- train.py
+++ train.py
@@ -45,42 +45,55 @@ if os.path.exists(os.path.join(checkpoints_dir, 'D.pkl')):
     print('loading D...')
     D = torch.load(os.path.join(checkpoints_dir, 'D.pkl'))
 
+loss_fn = torch.nn.BCELoss()
+
 G_optimizer = torch.optim.Adam(G.parameters(), lr=2e-4, betas=(0.5, 0.999))
 D_optimizer = torch.optim.Adam(D.parameters(), lr=2e-4, betas=(0.5, 0.999))
 
+# labels are not Variable!
+true_labels = torch.ones(batch_size).cuda()
+fake_labels = torch.zeros(batch_size).cuda()
+
+
+# noises are not Variable!
+#noises = torch.reshape(Variable(torch.from_numpy((np.vstack(torch.randn(100) for _ in range(batch_size))))), [batch_size, 100, 1, 1])
+noises = torch.randn(batch_size, 100, 1, 1)
+noises = noises.cuda()
+
 for epoch in range(num_epochs):
     for index, (image, _) in tqdm.tqdm(enumerate(dataloader)):
+        real_image = Variable(image)
+        real_image = real_image.cuda()
+
         if index % 1 == 0:
-            real_image = Variable(image)
-            real_image = real_image.cuda()
-
-            noises = torch.reshape(Variable(torch.from_numpy((np.vstack(torch.randn(100) for _ in range(batch_size))))), [batch_size, 100, 1, 1])
-            noises = noises.cuda()
-            
-            fake_image = G(noises).detach()
-
-            fake_pred = D(fake_image)
-            real_pred = D(real_image)
-
-            D_loss = - torch.mean(torch.log(real_pred)) - torch.mean(torch.log(1 - fake_pred))
-
             D_optimizer.zero_grad()
+            
+            real_pred = D(real_image)
+            D_loss_real = loss_fn(real_pred, true_labels)
+            D_loss_real.backward()
+
+
+            noises.data.copy_(torch.randn(batch_size, 100, 1, 1))
+            fake_image = G(noises).detach()
+            fake_pred = D(fake_image)
+            D_loss_fake = loss_fn(fake_pred, fake_labels)
+            D_loss_fake.backward()
+
+            #D_loss = - torch.mean(torch.log(real_pred)) - torch.mean(torch.log(1 - fake_pred))
+
             #D_loss.backward(retain_graph=True)
-            D_loss.backward()
+            #D_loss.backward()
             D_optimizer.step()
+            D_loss = D_loss_real + D_loss_fake
 
         if index % 5 == 0: 
-            noises = torch.reshape(Variable(torch.from_numpy((np.vstack(torch.randn(100) for _ in range(batch_size))))), [batch_size, 100, 1, 1])
-            noises = noises.cuda()
-            
-            fake_image = G(noises)
-
-            fake_pred = D(fake_image)
-
-            G_loss = torch.mean(torch.log(1 - fake_pred))
-
-
             G_optimizer.zero_grad()
+            
+            noises.data.copy_(torch.randn(batch_size, 100, 1, 1))
+            fake_image = G(noises)
+            fake_pred = D(fake_image)
+            #G_loss = torch.mean(torch.log(1 - fake_pred))
+            G_loss = loss_fn(fake_pred, true_labels)
             G_loss.backward()
             G_optimizer.step()
             
